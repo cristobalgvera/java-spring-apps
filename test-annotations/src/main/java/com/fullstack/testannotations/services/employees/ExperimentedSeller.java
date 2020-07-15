e3a6650e50a799e4b20cbf69c44e15ff8f52b180
Cristóbal Gajardo Vera <cristobalgajardo.v@gmail.com>
2020-07-15 16:53:10 -0400

    postconstruct and predestroy annotation uses

--- test-annotations/src/main/java/com/fullstack/testannotations/services/employees/ExperimentedSeller.java
+++ test-annotations/src/main/java/com/fullstack/testannotations/services/employees/ExperimentedSeller.java
@@ -6,8 +6,11 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 @Component // Annotation to reference a bean via class. This is used in main
-@Scope("prototype") // Annotation to define how object are created when it's requested. Default is singleton
+//@Scope("prototype") // Annotation to define how object are created when it's requested. Default is singleton
 public class ExperimentedSeller implements Employee {
 
     private Report report;
@@ -28,4 +31,26 @@ public class ExperimentedSeller implements Employee {
         // Here the class require a Report object, so Spring via @Autowired it's created
         return report.getReport();
     }
+
+    /*
+        Note:
+
+        @PostConstruct and @PreDestroy annotations can't be optimal executed if prototype scope is active
+        because Spring don't manage the complete lifecycle of a prototype bean, in contrast to the other
+        scopes.
+
+        These methods can be public, private or protected; can return any type of data (however, it's
+        commonly see them used like void methods) and they must not had arguments
+     */
+
+    @PostConstruct // Annotation to execute an action after create bean object instance
+    public void postBeanCreation() {
+        System.out.println("@PostConstruct annotation use");
+    }
+
+    @PreDestroy // Annotation to execute an action before destroy bean object instance
+    public void preBeanDestruction() {
+        System.out.println("@PreDestroy annotation use");
+    }
+
 }
