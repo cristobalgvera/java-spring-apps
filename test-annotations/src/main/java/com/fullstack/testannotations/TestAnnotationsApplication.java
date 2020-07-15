da1925e542ef7679a736c7001b30958c937d19ad
Cristóbal Gajardo Vera <cristobalgajardo.v@gmail.com>
2020-07-15 17:01:18 -0400

    configuration and componentscan annotation uses

--- test-annotations/src/main/java/com/fullstack/testannotations/TestAnnotationsApplication.java
+++ test-annotations/src/main/java/com/fullstack/testannotations/TestAnnotationsApplication.java
@@ -1,16 +1,18 @@
 package com.fullstack.testannotations;
 
+import com.fullstack.testannotations.configurations.AppConfig;
 import com.fullstack.testannotations.services.employees.Employee;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 @SpringBootApplication
 public class TestAnnotationsApplication {
 
     public static void main(String[] args) {
-        // Initialize Spring app
-        ApplicationContext applicationContext = SpringApplication.run(TestAnnotationsApplication.class, args);
+        // Initialize Spring app via @Configuration annotation
+        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
         // Request bean objects to Spring container
         Employee employee = (Employee) applicationContext.getBean("experimentedSeller");
         Employee employee1 = (Employee) applicationContext.getBean("experimentedSeller");
---
+++ test-annotations/src/main/java/com/fullstack/testannotations/configurations/AppConfig.java
@@ -0,0 +1,9 @@
+package com.fullstack.testannotations.configurations;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.fullstack.testannotations")
+public class AppConfig {
+}
