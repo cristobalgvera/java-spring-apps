a600c13b625b4740dc2d046e15265c32865f2d74
Cristóbal Gajardo Vera <cristobalgajardo.v@gmail.com>
2020-07-16 15:01:04 -0400

    requestparam annotation use

--- test-webapp/src/main/java/com/fullstack/testwebapp/controllers/GreetController.java
+++ test-webapp/src/main/java/com/fullstack/testwebapp/controllers/GreetController.java
@@ -3,8 +3,7 @@ package com.fullstack.testwebapp.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import javax.servlet.http.HttpServletRequest;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class GreetController {
@@ -19,14 +18,18 @@ public class GreetController {
     }
 
     @RequestMapping("/process-form-concatenated") // This change of name it's because we need to define an action for the <form> tag
-    public String processFormConcatenated(HttpServletRequest request, Model model) { // Note: Model object it's part of Spring
-        // Via request's parameters we call 'name' parameter and assign it to a variable
-        String alteredName = request.getParameter("name");
-        alteredName += " (this is a concatenated string)";
-        // Using this Model object called 'model' via .addAttribute(...) method we can insert
-        // parameters to be used by response view. Note that the first argument it's the name
-        // to reference it in .jsp page (this case: name = 'name') and the second one it's
-        // the object itself (this case: Object = (String) alteredName)
+    public String processFormConcatenated(@RequestParam("name") String alteredName, Model model) { // Note: Model object it's part of Spring
+        /*
+            Via @RequestParam("tag-name") now we can obtain values. This avoid to use request's
+            .getParameter() method simplifying our functions
+        */
+        alteredName += " (this is a concatenated string)"; // This value already had tag named 'name' value
+        /*
+            Using this Model object called 'model' via .addAttribute(...) method we can insert
+            parameters to be used by response view. Note that the first argument it's the name
+            to reference it in .jsp page (this case: name = 'name') and the second one it's
+            the object itself (this case: Object = (String) alteredName)
+        */
         model.addAttribute("name", alteredName);
         return "process-form";
     }
